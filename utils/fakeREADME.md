13aa68534ccbf05aa90eb22802ecc96c5cc2c12e
GitPit <rifflifter@gmail.com>
2021-06-24 19:39:50 -0400

    added ToC, email input, gitHub input

--- utils/fakeREADME.md
+++ utils/fakeREADME.md
@@ -1,1 +1,47 @@
 
+    # **title** README
+
+    Table of Contents
+    1. [ About. ] (#about)
+    2. [ Installation. ] (#install)
+    3. [ Usage. ] (#usage")
+    4. [ Contributors. ] (#contr)
+    5. [ Tests. ] (#test)
+    6. [ License. ] (#lice)
+
+    <a name="about"></a>
+    ##ABOUT
+    
+    descriptors
+    
+    <a name="install"></a>
+    ## INSTALLATION
+    
+    installation
+    
+    <a name="usage"></a>
+    ## USAGE
+    
+    usage users
+    
+    <a name="contr"></a>
+    ### Contributors
+    
+    contribute
+
+    [title](https://github.com/githim)
+
+    Email: email@email.com
+    
+    <a name="test"></a>
+    ### Tests
+    
+    test it
+    
+    <a name="lice"></a>
+    ### License
+    
+    undefined
+    
+    
+    
--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const tacoMaker = require('./generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -45,14 +45,28 @@ const promptUser = () => {
         },
 
         {
+            type: 'input',
+            name: 'gitHub',
+            message: 'What is your GitHub user name?',
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+
+        {
             type:'list',
             name: 'license',
             message: 'choose a license',
-      choices: ['MIT', 'Apache', 'Mozilla', 'ODC'],
+            choices: ['MIT', 'Apache', 'Mozilla', 'ODC'],
     }
 
 
     ])
+
+    
 }
 
 // TODO: Create a function to write README file
@@ -60,45 +74,63 @@ const promptUser = () => {
 
 const generateReadMe = (answers) => {
 
-    let license;
-
-    if (answers.license = license.choices[0]) license.badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    else if (answers.license = license.choices[1]) license.badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-    else if (answers.license = license.choices[2]) licsense.badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
-    else if (answers.license = license.choices[3]) license.badge = '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)';
     
-   
+    const badge = {name: 'link'};
+
+    if (license.choices == license.choices.choices[0]) badge.link = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    else if (license == license.choices.choices[1]) badge.link = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    else if (license == license.choices.choices[2]) badge.link = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    else if (license == license.choices.choices[3]) badge.link = '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)';
+
 
 
     console.log(answers)
     return `
-    #**${answers.title}**
+    # **${answers.title}** README
 
+    Table of Contents
+    1. [ About. ] (#about)
+    2. [ Installation. ] (#install)
+    3. [ Usage. ] (#usage")
+    4. [ Contributors. ] (#contr)
+    5. [ Tests. ] (#test)
+    6. [ License. ] (#lice)
+
+    <a name="about"></a>
     ##ABOUT
     
     ${answers.description}
     
+    <a name="install"></a>
     ## INSTALLATION
     
     ${answers.installation}
     
+    <a name="usage"></a>
     ## USAGE
     
     ${answers.usage}
     
+    <a name="contr"></a>
     ### Contributors
     
     ${answers.contributing}
+
+    [title](https://github.com/${answers.gitHub})
+
+    Email: ${answers.email}
     
+    <a name="test"></a>
     ### Tests
     
-    ${answers.test}
+    ${answers.tests}
     
+    <a name="lice"></a>
     ### License
     
-    ${answers.license}
+    ${answers.license.choices}
     
-    ${license.badge}
+    ${badge.link}
     `;
 }
 
@@ -117,24 +149,7 @@ init();
 
 
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
